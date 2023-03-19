81ae451e9c0d00b697198e621d8140cfd3dee564
Ahammed Rahees <ahdrahees.dev@gmail.com>
2023-03-19 22:35:09 +0530

    Added F/N & VRF

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -9,9 +9,11 @@
 
 pragma solidity 0.8.18;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Raffle__NotEnoughtETHEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     /* State Variable */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
@@ -19,7 +21,7 @@ contract Raffle {
     // Events
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
     }
 
@@ -35,10 +37,16 @@ contract Raffle {
         // name events with the function name reversed
     }
 
-    function pickRandomWinner() external {
+    function requestRandomWinner() external {
         // request a random number
         //nce we get it do something with it
+        // 2 transaction process
     }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
 
     /* view / pure functions */
     function getEntranceFee() public view returns (uint256) {
