b66c4e4b7da6a4bdc65fd043788ba54c525124bd
Ahammed Rahees <ahdrahees.dev@gmail.com>
2023-03-20 23:31:00 +0530

    Updated

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -10,6 +10,7 @@
 pragma solidity 0.8.18;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Raffle__NotEnoughtETHEntered();
 
@@ -17,12 +18,14 @@ contract Raffle is VRFConsumerBaseV2 {
     /* State Variable */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface COORDINATOR;
 
     // Events
     event RaffleEnter(address indexed player);
 
     constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinatorV2);
     }
 
     function enterRaffle() public payable {
