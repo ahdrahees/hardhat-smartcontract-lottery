19c4bae395cb59b6935f27b7164fe16522c6f21c
Ahammed Rahees <ahdrahees.dev@gmail.com>
2023-03-26 22:16:09 +0530

    Added namedAccounts

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,4 +9,15 @@ require("dotenv").config()
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: "0.8.18",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player_1: {
+            default: 1,
+        },
+        player_2: {
+            default: 2,
+        },
+    },
 }
