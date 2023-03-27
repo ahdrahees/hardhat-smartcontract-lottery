d5c023e3aefd374ed085b6e2b39589fccc51c513
Ahammed Rahees <ahdrahees.dev@gmail.com>
2023-03-27 22:21:26 +0530

    Modified

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,6 +6,14 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const POLYGON_RPC_URL = process.env.POLYGON_RPC_URL
+const GANACHE_RPC_URL = process.env.GANACHE_RPC_URL
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const GANACHE_PRIVATE_KEY = process.env.GANACHE_PRIVATE_KEY
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: "0.8.18",
@@ -18,6 +26,38 @@ module.exports = {
         },
         player_2: {
             default: 2,
+        },
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+            blockConfirmations: 6,
+        },
+        sepoli: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+            blockConfirmations: 6,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            // accounts hardhat automaticaly place
+            chainId: 31337,
+        },
+        polygon: {
+            url: POLYGON_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 137,
+            blockConfirmations: 6,
+        },
+        ganache: {
+            url: GANACHE_RPC_URL,
+            accounts: [GANACHE_PRIVATE_KEY],
+            chainId: 5777,
+            blockConfirmations: 6,
         },
     },
 }
