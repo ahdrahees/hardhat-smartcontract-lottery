8c9b218c65abb01f37a61452c6d4870fcaa95df6
Ahammed Rahees <ahdrahees.dev@gmail.com>
2023-03-28 21:06:45 +0530

    Modified

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,11 +1,15 @@
+const { network } = require("hardhat")
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
+
+    const args = []
 
     const raffle = await deploy("Raffle", {
         from: deployer,
         args: [],
         log: true,
-        waitConfirmations: 6,
+        waitConfirmations: network.config.blockConfirmations || 6,
     })
 }
