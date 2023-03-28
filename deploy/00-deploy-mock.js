de2f3fcec9e133288450bac6e3eeb0f4b166a61b
Ahammed Rahees <ahdrahees.dev@gmail.com>
2023-03-28 21:22:32 +0530

    modified

--- deploy/00-deploy-mock.js
+++ deploy/00-deploy-mock.js
@@ -18,3 +18,5 @@ module.exports = async ({ deployments, getNamedAccounts }) => {
         log("------------------Mocks---deployed------------------")
     }
 }
+
+// module.exports.tags = ["all", "mocks"]
