0eae3250c212cc205fef23cd81714fa86aefc33c
Ahammed Rahees <ahdrahees.dev@gmail.com>
2023-04-02 22:32:02 +0530

    ADDED consumer
    ADDED consumer for VRFCoordinatorV2Mock contract so we can request random number in the mocks

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -9,9 +9,10 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
     let vrfCoordinatorV2Address, subscriptionId
+    let vrfCoordinatorV2Mock
 
     if (developmentChains.includes(network.name)) {
-        const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
         vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
 
         const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
@@ -46,6 +47,14 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
+
+    if (developmentChains.includes(network.name)) {
+        await vrfCoordinatorV2Mock.addConsumer(subscriptionId, raffle.address)
+        // above line of code that will add deployed raffle contract as a consumer of the VRFCoordinatorV2Mock contract that will give random number. So we can request random number in mocks
+        // On a testnet or on mainnet, only contracts added as consumers can ask for randomness to the Coordinator, so we need to mimick this behavior as well in the mock, otherwise you'll get the InvalidConsumer error.
+
+        log("Consumer is added")
+    }
 
     // verify
     if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
