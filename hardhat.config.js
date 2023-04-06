780375837ca35d17b3dd6a2930774382ab7dd026
Ahammed Rahees <ahdrahees.dev@gmail.com>
2023-04-06 23:59:22 +0530

    Added etherscan, and some changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,15 +6,23 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
-const POLYGON_RPC_URL = process.env.POLYGON_RPC_URL
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL || "https://eth-goerli.g.alchemy.com/v2/YOUR-API-KEY"
+const SEPOLIA_RPC_URL =
+    process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY"
+const POLYGON_MAINNET_RPC_URL =
+    process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/YOUR-API-KEY"
 const GANACHE_RPC_URL = process.env.GANACHE_RPC_URL
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const GANACHE_PRIVATE_KEY = process.env.GANACHE_PRIVATE_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
+const GANACHE_PRIVATE_KEY = process.env.GANACHE_PRIVATE_KEY || "0x"
 
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key"
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "Your coinmarketcap API key"
+
+const REPORT_GAS = process.env.REPORT_GAS || false
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -32,17 +40,22 @@ module.exports = {
     },
     defaultNetwork: "hardhat",
     networks: {
+        hardhat: {
+            chainId: 31337,
+        },
         goerli: {
             url: GOERLI_RPC_URL,
             accounts: [PRIVATE_KEY],
             chainId: 5,
             blockConfirmations: 6,
+            saveDeployments: true,
         },
-        sepoli: {
+        sepolia: {
             url: SEPOLIA_RPC_URL,
             accounts: [PRIVATE_KEY],
             chainId: 11155111,
             blockConfirmations: 6,
+            saveDeployments: true,
         },
         localhost: {
             url: "http://127.0.0.1:8545/",
@@ -50,10 +63,11 @@ module.exports = {
             chainId: 31337,
         },
         polygon: {
-            url: POLYGON_RPC_URL,
+            url: POLYGON_MAINNET_RPC_URL,
             accounts: [PRIVATE_KEY],
             chainId: 137,
             blockConfirmations: 6,
+            saveDeployments: true,
         },
         ganache: {
             url: GANACHE_RPC_URL,
@@ -62,8 +76,16 @@ module.exports = {
             blockConfirmations: 6,
         },
     },
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY,
+            goerli: ETHERSCAN_API_KEY,
+            polygon: POLYGONSCAN_API_KEY,
+        },
+    },
     gasReporter: {
-        enabled: false,
+        enabled: REPORT_GAS,
         outputFile: "gas-reporter.txt",
         noColors: true,
         currency: "USD",
@@ -71,6 +93,6 @@ module.exports = {
         token: "ETH",
     },
     mocha: {
-        setTimeout: 300000, // 300 sec
+        timeout: 500000, // 500 seconds max for running tests
     },
 }
