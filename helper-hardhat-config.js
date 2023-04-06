e2d050f2640bc26e35989e1eba27435a7435d73e
Ahammed Rahees <ahdrahees.dev@gmail.com>
2023-04-07 00:01:02 +0530

    Added subscriptionId

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -6,16 +6,16 @@ const networkConfig = {
         vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
         entranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 150 gwei Key Hash
-        subscriptionId: "0",
+        subscriptionId: "7499",
         callBackGasLimit: "500000", // 500,000
         interval: "30",
     },
-    1115511: {
+    11155111: {
         name: "sepolia",
-        vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
+        vrfCoordinatorV2: "0x8103b0a8a00be2ddc778e6e7eaa21791cd364625",
         entranceFee: ethers.utils.parseEther("0.01"),
         gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei Key Hash
-        subscriptionId: "0",
+        subscriptionId: "1036",
         callBackGasLimit: "500000",
         interval: "30", //seconds
     },
